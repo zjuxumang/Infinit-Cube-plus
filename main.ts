4c4b5ac570a01ccf4b12066281d75b61c46c60e5
zhu zhuangtian <576583342@qq.com>
2019-08-07 07:11:37 +0800

    0807-3

--- main.ts
+++ main.ts
@@ -4,21 +4,3 @@ pins.P16.digitalWrite(true);
 basic.pause(10);
 Cube.Init();
 basic.showIcon(IconNames.Happy);
-
-while (1)
-{
-    Cube.Motor(Cube.Motor_ID.M1, Cube.Motor_Dir.Forward, 70);
-    Cube.Motor(Cube.Motor_ID.M2, Cube.Motor_Dir.Forward, 70);
-    basic.pause(1000);
-    Cube.Motor(Cube.Motor_ID.M1, Cube.Motor_Dir.Forward, 0);
-    Cube.Motor(Cube.Motor_ID.M2, Cube.Motor_Dir.Forward, 0);
-    basic.pause(1000);
-    Cube.Motor(Cube.Motor_ID.M1, Cube.Motor_Dir.Backward, 70);
-    Cube.Motor(Cube.Motor_ID.M2, Cube.Motor_Dir.Backward, 70);
-    basic.pause(1000);
-    Cube.Motor(Cube.Motor_ID.M1, Cube.Motor_Dir.Forward, 0);
-    Cube.Motor(Cube.Motor_ID.M2, Cube.Motor_Dir.Forward, 0);
-    basic.pause(1000);
-    // Cube.Init();
-    // basic.pause(1000);
-}
